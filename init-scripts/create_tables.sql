4df6c3beab7d50a363f3ba5c85bd44f6ba3a23dd
nathan <nathan-nunes@hotmail.com>
2023-02-26 01:34:39 -0500

    AB#85 created docker image and repo

---
+++ init-scripts/create_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE course_catalog (
+id MEDIUMINT NOT NULL AUTO_INCREMENT,
+code VARCHAR(10) NOT NULL,
+name VARCHAR(30) ,
+prerequisites JSON,
+PRIMARY KEY(id)
+);
---
+++ init-scripts/insert_data.sql
@@ -0,0 +1,1 @@
+INSERT INTO course_catalog(id,code,name) VALUES (1,"CPSC-6030","Data Visualization");
