c979d14bf6e71e0a9d867e7cbb626450937f61a4
nathan <nathan-nunes@hotmail.com>
2023-02-27 16:16:27 -0500

    AB#85 added concentrarion column

--- init-scripts/create_tables.sql
+++ init-scripts/create_tables.sql
@@ -2,6 +2,7 @@ CREATE TABLE course_catalog (
 id MEDIUMINT NOT NULL AUTO_INCREMENT,
 code VARCHAR(10) NOT NULL,
 name VARCHAR(30) ,
+concentration VARCHAR(25),
 prerequisites JSON,
 PRIMARY KEY(id)
 );
--- init-scripts/insert_data.sql
+++ init-scripts/insert_data.sql
@@ -1,1 +1,1 @@
-INSERT INTO course_catalog(id,code,name) VALUES (1,"CPSC-6030","Data Visualization");
+INSERT INTO course_catalog(id,code,name) VALUES (1,"CPSC-6030","Data Visualization","Data and Informatics",'["STAT 2300"]');
